d3b3f8d1d1afb21fbc02661598e721bdbceb9d5c
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-07-18 13:03:57 +0100

    add medical_histories patients and treatments table establish relation between them

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,33 @@
+/* Database schema to keep the structure of entire database. */
+
+/* Create table medical_histories*/
+CREATE TABLE medical_histories (
+    id serial primary key,
+    admitted_at timestamp,
+    patient_id int,
+    status varchar(100),
+    FOREIGN KEY (patient_id) REFERENCES patients (id)
+);
+
+/* Create table patient*/
+CREATE TABLE patients(
+    id serial primary key,
+    name varchar(100),
+    date_of_birth date
+);
+
+/* Create table treatments*/
+CREATE TABLE treatments(
+    id serial primary key,
+    type varchar(100),
+    name varchar(100)
+);
+/* establish the many-to-many relationship between medical histories and treatments*/
+
+CREATE TABLE medical_history_treatments (
+    id serial primary key,
+    medical_history_id int,
+    treatment_id int,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+);
